350de6c0a4f39a41b7c2e27859644a1bc6b3db50
1090dopl <96866387+1090dopl@users.noreply.github.com>
2022-04-21 20:45:04 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,7 @@ class User < ApplicationRecord
       validates :password,format:{with:PASSWORD_REGEX,message:'Include both letters and numbers'},presence: true,length:{minimum:6}
    
       with_options presence: true do
-     validates :email, presence: true,format:{with:/[A-Za-z0-9._+]+@[A-Za-z]+.[A-Za-z]/},uniqueness: true
-     validates :nickname,presence: true, uniqueness:true
+     validates :nickname,presence: true
      validates :first_name, presence: true
      validates :first_name,format:{with:/\A[ぁ-んァ-ン一-龥]/}
      validates :last_name, presence: true
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-# ActiveSupport::Reloader.to_prepare do
+# ActiveSupport::Deprecation .silenced=true if Rails.version
 #   ApplicationController.renderer.defaults.merge!(
 #     http_host: 'example.org',
 #     https: false
