896d7e8952d18c5fa4e9b7bdc93a51825deab01c
1090dopl <96866387+1090dopl@users.noreply.github.com>
2022-04-24 12:57:25 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,9 @@ class User < ApplicationRecord
       with_options presence: true do
      validates :nickname,presence: true
      validates :first_name, presence: true
-     validates :first_name,format:{with:/\A[ぁ-んァ-ン一-龥々-]/}
+     validates :first_name,format:{with:/\A[ぁ-んァ-ン一-龥々-]+\z/}
      validates :last_name, presence: true
-     validates :last_name,format:{with:/\A[ぁ-んァ-ン一-龥々-]/}
+     validates :last_name,format:{with:/\A[ぁ-んァ-ン一-龥々-]+\z/}
      validates :last_name_kana, presence: true
      validates :last_name_kana, format:{with:/\A[ぁ-んァ-ヶー－]+\z/}
      validates :first_name_kana, presence: true
