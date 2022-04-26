0954fe8cc12790859e13f4e8018f9881ba50261d
1090dopl <96866387+1090dopl@users.noreply.github.com>
2022-04-26 19:40:57 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,32 @@
 class ItemsController < ApplicationController
   def index
-    
+    @item=Item.all
   end
+
+  def new
+    @item=Item.new
+  end
+
+  def create
+    @item=Item.new(item_params)
+    
+    @item.save
+    redirect_to root_path
+  end
+
+  private
+   def move_to_signed_in
+    unless user_signed_in?
+      redirect_to '/users/sign_in'
+    end
+  end
+
+  def item_params
+    params.require(:item).permit(:name,:image,:explanation,:price,:category_id,:status_id,:charge_id,:prefecture_id,:ship_date_id).merge(user_id:current_user.id)
+  end
+end
+
+
+
+
 end
