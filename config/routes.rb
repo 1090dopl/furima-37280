7d268539eb15c6b6dca59f2ceb6771ecc435e305
1090dopl <96866387+1090dopl@users.noreply.github.com>
2022-05-03 15:05:37 +0900

    indexアクションへのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root to:'items#index'
-  resources :items     
+  resources :items, only:[:new,:create]do
+    resources :purchases,only:[:new,:create]    
   end
+end
 
 
 
