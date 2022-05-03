6645753df2fa484053fcc8e0c585df98e9a07a6d
1090dopl <96866387+1090dopl@users.noreply.github.com>
2022-05-03 11:31:01 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!,expect:[:index,:show]
+  before_action :authenticate_user!,except:[:index,:show]
   before_action :set_item,only:[:show,:edit,:update,:destroy]
+  
 
   def index
     @items=Item.all
@@ -19,6 +20,13 @@ class ItemsController < ApplicationController
     end
   end
  
+ def edit
+   if @item.user_id !=current_user.id
+   else
+    redirect_to root_path
+   end
+  end
+
   
 
   private
