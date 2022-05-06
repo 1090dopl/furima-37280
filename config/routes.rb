1aa5dd990c01f435e9c4c2e055ad552be26084b1
1090dopl <96866387+1090dopl@users.noreply.github.com>
2022-05-06 16:39:26 +0900

    商品情報を表示する

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to:'items#index'
-  resources :items, only:[:new,:create]do
-    resources :purchases,only:[:new,:create]    
+  resources :items do
+  
+    
   end
 end
 
