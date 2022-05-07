5bb103dea91f69a6a1ae6d59cd5eceb74072980c
1090dopl <96866387+1090dopl@users.noreply.github.com>
2022-05-07 16:46:28 +0900

    商品詳細画面で商品情報と画像を表示する

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,39 +1,45 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!,only:[:new,:create]
+  before_action :authenticate_user!,only:[:index,:show]
   before_action :set_item,only:[:show,:edit,:update,:destroy]
   
 
   def index
-    @items=Item.all.order(created_at: :desc)
+    @items=Item.all
+    @items=Item.order(created_at: :desc)
     
   end
 
   def new
-    @item=Item.new
+    if user_signed_in?
+       @item=Item.new
+    else
+      redirect_to user_session_path(@item.id)
   end
+end
 
   def create
     @item=Item.new(item_params)
     if @item.save
-     redirect_to root_path
+     redirect_to items_path
     else
       render :new
     end
   end
  
  def show
+  @item = Item.find(params[:id])
  end
 
 
 
 
  def edit
-   #if @item.user_id !=current_user.id
-   #else
-    #redirect_to root_path
+   if @item.user_id ==current_user.id&&@item.purchase.nil
+   else
+    redirect_to root_path
    
   end
-
+end
   
 
   private
---
+++ app/models/purchase.rb
@@ -0,0 +1,5 @@
+class OrderContoller< ApplicationRecord
+  belongs_to :user
+  belongs_to :item
+  #has_one :address
+end
