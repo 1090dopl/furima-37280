7bb02907c05d953c81a0df0ae418b726dbda3d21
1090dopl <96866387+1090dopl@users.noreply.github.com>
2022-05-24 20:26:52 +0900

    Revert "Revert "クレジット決済機能実装""
    This reverts commit 43ed02d885229ad156df26784c61dc97351df271.

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,22 +1,22 @@
 class OrderForm
   include ActiveModel::Model
-  attr_accessor :user_id,:item_id, :city,:buinding_name,:buinding_number,:phone_number,:postal_code,:prefecture_id,:price,:token,:house_number
+  attr_accessor :user_id,:item_id, :city,:building_name,:phone_number,:postal_code,:prefecture_id,:token,:house_number
   
   with_options presence:  true do
    validates :user_id
    validates :item_id
    validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
-   validates :price, numericality:{only_interger: true, greater_than_or_equal_to: 1,less_than_or_equal_to: 1000000,message: 'is invalid'}
    validates :city
    validates :postal_code, format:{with:/\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
    validates :prefecture_id,numericality:{other_than: 0,message:"can't be blank"}
    validates :token
+   validates :house_number
   end
 
 def save
   order=Order.create(user_id: user_id,item_id: item_id)
  
-  Address.create(order_id: order.id,postal_code: postal_code,prefecture_id: prefecture_id,city: city,buinding_number: buinding_number,house_number: house_number,phone_number: phone_number)
+  Address.create(order_id: order.id,postal_code: postal_code,prefecture_id: prefecture_id,city: city,building_name: building_name,house_number: house_number,phone_number: phone_number)
 
   end
 
--- db/migrate/20220510112321_create_orders.rb
+++ db/migrate/20220510112321_create_orders.rb
@@ -1,8 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-     t.refenences :user
-     t.refenences :item
+     t.refenences :user ,null: false, foregin_key: true
+     t.refenences :item ,null: false, foregin_key: true
       t.timestamps
     end
   end
