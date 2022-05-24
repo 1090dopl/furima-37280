1bfc73a80f2d4c582104b8747e1eda7d19350f5a
1090dopl <96866387+1090dopl@users.noreply.github.com>
2022-05-24 20:22:13 +0900

    Revert "購入機能実装"
    This reverts commit c7c6f4e0a5b5d550b9982d3447067b1a93c69161.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
       
       #has_many :items
-      #has_many :orders
+      #has_many :purchases
     
       
       PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
