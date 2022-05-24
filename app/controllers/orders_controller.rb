43ed02d885229ad156df26784c61dc97351df271
1090dopl <96866387+1090dopl@users.noreply.github.com>
2022-05-24 20:21:15 +0900

    Revert "クレジット決済機能実装"
    This reverts commit 2b866c6b7ef97876263a1be55ca6d366d069025e.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,27 +16,17 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_form = OrderForm.new(order_params)
     if @order_form.valid?
-      pay_item
       @order_form.save
        return redirect_to root_path
     else
-      render :index
+      render 'index'
     end
   end
 
   private
    
   def order_params
-    params.require(:order_form).permit(:postal_code,:prefecture_id,:city,:house_number,:building_name,:phone_number).merge(user_id: current_user.id,item_id:params[:item_id],token:params[:token])
+    params.require(:order_form).permit(:postal_code,:prefecture,:city,:house_number,:building_name,:phone_number).merge(user_id: current_user.id,item_id:params[:item_id],token:params[:token])
   end
-
-  def pay_item
-     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-     Payjp::Charge.create(
-        amount: @item.price,
-        card: order_params[:token],
-        currency: 'jpy'
-     )
-    end
 
 end
--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,35 +1,9 @@
 const pay =() => {
-  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
-  const form = document.getElementById("charge-form");
-  form.addEventListener("submit",(e) =>{
+  const form = document.getElementById("charge-form")
+  submit.addEventListener("submit",(e) =>{
     e.preventDefault();
   const formResult = document.getElementById("charge-form");
-  const formData = new FormData(formResult);
-
-   const card ={
-     number: formData.get("order_form[number]"),
-     cvc: formData.get("order_form[cvc]"),
-     exp_month: formData.get("order_form[exp_month]"),
-     exp_year: `20${formData.get("order_form[exp_year]")}`,
-   };
-   console.log(card);
-
-  Payjp.createToken(card,(status,response) =>{
-    if(status == 200) {
-      const token = response.id;
-      const renderDom = document.getElementById("charge-form");
-      const tokenobj = `<input value=${token} name='token' type="hidden">`;
-       renderDom.insertAdjacentHTML("beforeend",tokenobj);
-    }
-
-    document.getElementById("card-number").removeAttribute("name");
-    document.getElementById("card-cvc").removeAttribute("name");
-    document.getElementById("card-exp-month").removeAttribute("name");
-    document.getElementById("card-exp-year").removeAttribute("name");
-
-    //document.getElementById("charge-form").submit();
-  });
-});
+  const formDate = new FormData(formResult);
 
 };
 
