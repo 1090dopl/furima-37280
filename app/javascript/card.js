27c5e5179345c32a96ffa7a2313ed72b3f25e284
1090dopl <96866387+1090dopl@users.noreply.github.com>
2022-05-24 20:26:49 +0900

    Revert "Revert "購入機能実装２""
    This reverts commit a6749184277b24e84d490308dade16676119fc3c.

---
+++ app/javascript/card.js
@@ -0,0 +1,10 @@
+const pay =() => {
+  const form = document.getElementById("charge-form")
+  submit.addEventListener("submit",(e) =>{
+    e.preventDefault();
+  const formResult = document.getElementById("charge-form");
+  const formDate = new FormData(formResult);
+
+};
+
+window.addEventListener("load",pay);
--- db/migrate/20220510112321_create_orders.rb
+++ db/migrate/20220510112321_create_orders.rb
@@ -1,7 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-
+     t.refenences :user
+     t.refenences :item
       t.timestamps
     end
   end
