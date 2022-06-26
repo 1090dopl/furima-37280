04e647dac529d2570d5a51ce608de1ac9ef57e35
1090dopl <96866387+1090dopl@users.noreply.github.com>
2022-06-26 17:32:24 +0900

    Update user_spec.rb

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -89,26 +89,26 @@ RSpec.describe User, type: :model do
         @user.password = "1234a"
         @user.password_confirmation="1234a"
         @user.valid?
-        expect(@user.errors.full_messages).to include('パスワードは６文字以上で入力してください')
+        expect(@user.errors.full_messages).to include('パスワード（確認含む）は６文字以上で入力してください')
       end
       it 'password(確認含む）が半角英数字でなければ登録できない' do
         @user.password = '123456'
         @user.password_confirmation='123456'
         @user.valid?
-        expect(@user.errors.full_messages).to include("パスワードは不正な値です")
+        expect(@user.errors.full_messages).to include("パスワード(確認含む）を半角英数字で入力してください")
       end
       it 'passwordは半角英字のみでは登録できない' do
        @user.password='abcdef'
        @user.password_confirmation='abcdef'
        @user.valid?
-       expect(@user.errors.full_messages).to include("パスワードは不正な値です")
+       expect(@user.errors.full_messages).to include("パスワード（確認含む）は半角英数字で入力してください")
     end
 
       it 'passwordは半角数字のみでは登録できない' do
        @user.password='123456'
        @user.password_confirmation='123456'
        @user.valid?
-       expect(@user.errors.full_messages).to include("パスワードは不正な値です")
+       expect(@user.errors.full_messages).to include("パスワード（確認含む）は半角英数字で入力してください")
     end
       it 'passwordとpassword_confirmationが不一致では登録できないこと' do
         @user.password = '12345a'
@@ -135,12 +135,12 @@ RSpec.describe User, type: :model do
         it 'first_name_kanaのフリガナは全角（カタカナ)でなければ登録できない' do
           @user.first_name_kana='陸太郎'
           @user.valid?
-          expect(@user.errors.full_messages).to include("名前カナは全角で入力してください")
+          expect(@user.errors.full_messages).to include("名前カナを入力してください")
       end
       it 'last_name_kanaのフリガナは全角（カタカナ)でなければ登録できない' do
          @user.last_name_kana='山田'
          @user.valid?
-         expect(@user.errors.full_messages).to include("苗字カナは不正な値です")
+         expect(@user.errors.full_messages).to include("苗字カナを入力してください")
       end
       it 'first_name_kanaが空だと登録できない' do
         @user.first_name_kana=''
